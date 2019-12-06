481a0a49e9ced88fe88e27af1117c670e4174fb4
Pawel <pawel00100@gmail.com>
2019-12-06 19:19:14 +0100

    Slider updated

---
+++ src/main/runner/Run.java
@@ -0,0 +1,23 @@
+package main.runner;
+
+import main.map.RectangularMap;
+
+public class Run {
+    RectangularMap map;
+
+    public Run(RectangularMap map){
+        this.map = map;
+
+        while (true){
+            new Step(map);
+            try{
+                Thread.sleep(500);
+            }
+            catch (Exception E){}
+        }
+
+    }
+
+
+
+}
---
+++ src/main/runner/Step.java
@@ -0,0 +1,12 @@
+package main.runner;
+
+import main.map.RectangularMap;
+
+class Step {
+    RectangularMap map;
+
+    Step(RectangularMap map){
+        this.map = map;
+        System.out.println("aa");
+    }
+}
