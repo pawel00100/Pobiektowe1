66c1bc7a0737898b631c782b7f38192d83f07d89
Pawel <pawel00100@gmail.com>
2019-12-20 14:57:05 +0100

    animal generation change

--- src/main/World.java
+++ src/main/World.java
@@ -3,19 +3,22 @@ package main;
 import main.map.RectangularMap;
 import main.mapElements.Animal;
 import main.UI.UI;
+import main.mapElements.Vector2d;
 import main.parser.Parser;
 import main.runner.Run;
 import org.json.JSONObject;
 
 import java.io.IOException;
 
-public class World {
+public class World { ;
     public static void main(String[] Argc) {
         try {
             JSONObject parameters = new Parser().obj;
 
             RectangularMap map1 = new RectangularMap(parameters);
             RectangularMap map2 = new RectangularMap(parameters);
+
+            generateAnimals(parameters.getInt("initialAnimals"), map1, map2);
 
             new UI(map1, map2, parameters);
 
@@ -24,7 +27,21 @@ public class World {
         catch (Exception exception){
             System.out.println(exception.getMessage());
         }
+    }
 
+
+    static void generateAnimals(int number, RectangularMap map1, RectangularMap map2){
+        for (int i = 0; i < number; i++) {
+            Vector2d position = generateRandomPosition(map1);
+            new Animal(map1, position);
+            new Animal(map2, position);
+        }
+    }
+
+    static Vector2d generateRandomPosition(RectangularMap map){
+        int xOffset = (int) ((map.upperBoundary.x - map.lowerBoundary.x + 1) * Math.random());
+        int yOffset = (int) ((map.upperBoundary.y - map.lowerBoundary.y + 1) * Math.random());
+        return new Vector2d(xOffset + map.lowerBoundary.x, yOffset + map.lowerBoundary.y);
     }
 }
 
